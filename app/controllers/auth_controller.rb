1b94059ea9b368063eefa539deedc6589d7b9a15
Ricardo Rivas <richistron@gmail.com>
2023-06-18 09:54:28 -0700

    fixing auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,13 +6,28 @@ class AuthController < ApplicationController
   def login
     return invalid_login_response unless @user.authenticate login_params[:password]
 
-    render status: :ok, json: serialize(@user)
+    create_and_destroy_expired
+    render status: :ok, json: serialize(active_api_keys.first)
   end
 
   private
 
   def login_params
     params.permit(:username, :password)
+  end
+
+  def create_and_destroy_expired
+    expired_keys.destroy_all
+
+    @user.api_keys.create! if active_api_keys.count.zero?
+  end
+
+  def expired_keys
+    @expired_keys ||= @user.api_keys.expired_keys
+  end
+
+  def active_api_keys
+    @active_api_keys ||= @user.api_keys.active_keys
   end
 
   def find_user
@@ -25,7 +40,7 @@ class AuthController < ApplicationController
     render status: :bad_request, json: { error: 'invalid username/password combination' }
   end
 
-  def serialize(user)
-    user.as_json only: %i[uuid username]
+  def serialize(api_key)
+    api_key.as_json only: %i[token expiration]
   end
 end
--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -20,31 +20,51 @@ RSpec.describe 'Auth', type: :request do
         let(:body) { JSON.parse(response.body) }
 
         it 'does not return id and password digest' do
-          not_included = %w[id password_digest password password_confirmation created_at updated_at]
+          not_included = %w[id created_at updated_at]
           expect(body).not_to include(*not_included)
         end
 
         it 'should include uuid and username' do
-          included = %w[uuid username]
+          included = %w[token expiration]
           expect(body).to include(*included)
         end
       end
     end
 
-    context 'login failed' do
-      before do
-        post '/auth/login', params: { username: user.username, password: '123' }
+    context 'ApiKeys rotation' do
+      after { Timecop.return }
+
+      it 'returns new api_key when the old one is expired' do
+        user.api_keys.create!
+        old_token = user.api_keys.first.token
+        Timecop.travel(Time.now + 8.hours)
+        post '/auth/login', params: { username: user.username, password: }
+        expect(JSON.parse(response.body)['token']).to_not eq(old_token)
       end
 
-      it 'returns http bad_request' do
-        expect(response).to have_http_status(:bad_request)
+      it 'destroys old one if expired' do
+        user.api_keys.create!
+        old_token = user.api_keys.first.token
+        Timecop.travel(Time.now + 8.hours)
+        post '/auth/login', params: { username: user.username, password: }
+        expect(ApiKey.find_by(token: old_token)).to be_nil
       end
 
-      context 'body response' do
-        let(:body) { JSON.parse(response.body) }
+      context 'login failed' do
+        before do
+          post '/auth/login', params: { username: user.username, password: '123' }
+        end
 
-        it 'does not return id and password digest' do
-          expect(body['error']).to eq('invalid username/password combination')
+        it 'returns http bad_request' do
+          expect(response).to have_http_status(:bad_request)
+        end
+
+        context 'body response' do
+          let(:body) { JSON.parse(response.body) }
+
+          it 'does not return id and password digest' do
+            expect(body['error']).to eq('invalid username/password combination')
+          end
         end
       end
     end
